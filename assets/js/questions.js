4cd414beb36c6a9b138b8fb49ca1230cbbb6717b
Kendra Staver <staverkendra@gmail.com>
2021-08-30 14:16:56 -0700

    Deciphered questions from gif and added them

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,8 @@
 var questions = [
-    
+    "Commonly used data types do not include: "
+    "The condition in an if/then statement is enclosed with: "
+    "Arrays in JavaScript can be used to store "
+    "String values must be enclosed within " " when being assigned to variables."
+    "A very useful tool used during development and debugging for printing content to the debugger is: "
+
 ]
