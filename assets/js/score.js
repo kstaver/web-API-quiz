4481956544c48c0101c62b59d2878c477af86540
Kendra Staver <staverkendra@gmail.com>
2021-08-31 22:32:07 -0700

    finished script.js

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,7 +1,32 @@
 function displayHighScores(){
 
+    
+    var highScores = JSON.parse(windows.localStorage.getItem("highScores")) || [];
+
+    // Sort the high scores from highest to lowest.
+    highScores.sort(function(a, b){
+
+        return b.score - a.score;
+    });
+
+    // Assign a tag to each score and display those scores
+    highScores.forEach(function(score){
+
+        // Tag assignment
+        var highScoresTag = document.createElement("highScoresTag");
+        highScoresTag.textContent = score.initials + " - " + score.score;
+
+        // Display tags
+        var displayHighScoresEl = document.getElementById("highScores");
+        displayHighScoresEl.appendChild(highScoresTag);
+    });
 }
 
+// Remove all high scores from the list of high scores
 function clearHighScores(){
 
+    window.localStorage.removeItem("highScores");
+    window.location.reloard();
 }
+
+printHighScores();
